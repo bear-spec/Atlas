fb2208a4cdd2b53392a10256a003e35980271b0d
bear-spec <iamrathin578@gmail.com>
2026-02-06 22:17:49 +0530

    Add AboutUs component for nursery information

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,19 @@
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery is an online plant store dedicated to providing
+        healthy, high-quality plants for homes and gardens. We believe
+        that plants bring life, beauty, and positivity into everyday living.
+      </p>
+      <p>
+        Our mission is to make plant shopping easy and enjoyable by offering
+        a wide variety of plants with a simple and interactive shopping
+        experience.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
