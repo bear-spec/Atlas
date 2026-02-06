c4a47d297c71890bc5e7f8d394f93f775c716180
bear-spec <iamrathin578@gmail.com>
2026-02-06 22:19:01 +0530

    Add initial App component with landing page

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="landing-page">
+      <div className="overlay">
+        <h1>Paradise Nursery</h1>
+        <p>
+          Discover beautiful and healthy plants to brighten your home
+          and garden.
+        </p>
+        <button className="get-started-btn">Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
